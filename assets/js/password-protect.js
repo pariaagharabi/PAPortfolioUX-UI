54bc681a4ec28ceb3dc5b22272ea7e3f8b2e1ace
PARIA AGHARABI <pariaagharabi@gmail.com>
2024-12-26 11:22:49 -0800

    adding js to manage redirect

--- assets/js/password-protect.js
+++ assets/js/password-protect.js
@@ -1,15 +1,36 @@
+function setRedirectUrl() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const redirectUrl = urlParams.get('redirect');
+
+    if (redirectUrl) {
+        document.getElementById('link').href = redirectUrl;
+    }
+
+    if (window.history.replaceState) {
+        const urlWithoutQuery = window.location.protocol + "//" + window.location.host + window.location.pathname;
+        window.history.replaceState({ path: urlWithoutQuery }, '', urlWithoutQuery);
+    }
+}
+
+function checkEnter(event) {
+    if (event.key === 'Enter') {
+        enterPassword();
+    }
+}
+
 let enterPassword = function () {
     let inputVal = document.getElementById('input').value;
-    let pw = "2020";
-    let correct = "Password correct. You may view my portfolio";
-    let incorrectRed = "Password incorrect. Try again.";
-    let incorrect = incorrectRed.fontcolor("red");
+    let pw = "paria2025";
+    let incorrect = "<span style='color: red'>Password incorrect. Try again.</span>";
     if (inputVal === pw) {
-        document.getElementById('message').innerHTML = correct;
+        document.getElementById('message').innerHTML = "";
         document.getElementById('hidden').style.display = "block";
-        // document.getElementById('requestPW').style.display = "none";
+        window.location.href = document.getElementById('link').href;
     } else {
         document.getElementById('message').innerHTML = incorrect;
         document.getElementById('hidden').style.display = "none";
     }
 }
+
+setRedirectUrl();
