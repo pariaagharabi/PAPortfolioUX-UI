918ad1095c7681997f8455d4cbb9fdc01699536b
PARIA AGHARABI <pariaagharabi@gmail.com>
2025-01-17 12:17:05 -0800

    Added js file for play video

--- assets/js/video-scripts.js
+++ assets/js/video-scripts.js
@@ -1,19 +1,76 @@
-const videoBtn = document.getElementById('videoBtn');
-const videoPopup = document.getElementById('videoPopup');
-const overlay = document.getElementById('overlay');
-const closeBtn = document.getElementById('closeBtn');
+document.addEventListener("DOMContentLoaded", function (event) {
 
-videoBtn.onclick = function () {
-    videoPopup.style.display = 'block';
-    overlay.style.display = 'block';
-}
+    // Get all videos
+    var videos = document.getElementsByClassName("video__play-button");
+    var video_fullscreen = document.getElementsByClassName("video__fullscreen-button");
 
-closeBtn.onclick = function () {
-    videoPopup.style.display = 'none';
-    overlay.style.display = 'none';
-}
+    // Loop through all videos
+    for (var i = 0; i < videos.length; i++) {
+        // On click play button
+        videos[i].onclick = function () {
 
-overlay.onclick = function () {
-    videoPopup.style.display = 'none';
-    overlay.style.display = 'none';
-}
+            // Find video element
+            video = this.nextElementSibling;
+            video_wrapper = this.parentElement;
+            console.log(video_wrapper);
+            console.log(video);
+
+            // Play video
+            if (video.paused == true) {
+                // Play the video
+                video.play();
+                video.setAttribute('data-state', 'play');
+                video.setAttribute('data-state-reload', 'false');
+                video_wrapper.setAttribute('data-state', 'play');
+
+                // Update the button text to 'Pause'
+                // this.innerHTML = "Pause";
+                this.setAttribute('data-button-state', 'play');
+                this.setAttribute('data-button-state-reload', 'false');
+            } else {
+                // Pause the video
+                video.pause();
+                video.setAttribute('data-state', 'pause');
+                video_wrapper.setAttribute('data-state', 'pause');
+
+                // Update the button text to 'Play'
+                // this.innerHTML = "Play";
+                this.setAttribute('data-button-state', 'pause');
+            }
+
+            // When video is ended
+            video.onended = (event) => {
+                // Pause the video
+                video.setAttribute('data-state', 'pause');
+                video.setAttribute('data-state-reload', 'true');
+
+                // Update the button text to 'Play'
+                this.setAttribute('data-button-state', 'pause');
+                this.setAttribute('data-button-state-reload', 'true');
+            };
+
+        }
+
+    }
+
+
+    // Loop through all videos
+    for (var i = 0; i < video_fullscreen.length; i++) {
+        // On click play button
+        video_fullscreen[i].onclick = function () {
+
+            // Find video element
+            video = this.nextElementSibling.nextElementSibling;
+
+            if (video.mozRequestFullScreen) {
+                video.mozRequestFullScreen();
+            } else if (video.webkitRequestFullScreen) {
+                video.webkitRequestFullScreen();
+            }
+
+        }
+
+    }
+
+});
+
